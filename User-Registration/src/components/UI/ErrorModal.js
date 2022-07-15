0ae4813d71aa014f5d0e2ef263bf41c0186a367f
Martynas Goberis <85226477+Margo17@users.noreply.github.com>
2022-07-15 19:30:16 +0300

    Refactor with fragments, portals and refs

--- User-Registration/src/components/UI/ErrorModal.js
+++ User-Registration/src/components/UI/ErrorModal.js
@@ -1,27 +1,45 @@
+import ReactDOM from 'react-dom';
+
 import Card from './Card';
 import Button from './Button';
 import classes from './ErrorModal.module.css';
 
-const ErrorModal = (props) => {
-	const dismissHandler = () => {
-		props.onDismiss();
-	};
+const Backdrop = (props) => {
+	return <div className={classes.backdrop} onClick={props.onDismiss} />;
+};
 
+const ModalOverlay = (props) => {
 	return (
-		<div>
-			<div className={classes.backdrop} onClick={dismissHandler}></div>
-			<Card className={classes.modal}>
-				<header className={classes.header}>
-					<h2>{props.title}</h2>
-				</header>
-				<div className={classes.content}>
-					<p>{props.message}</p>
-				</div>
-				<footer className={classes.actions}>
-					<Button onClick={dismissHandler}>Okay</Button>
-				</footer>
-			</Card>
-		</div>
+		<Card className={classes.modal}>
+			<header className={classes.header}>
+				<h2>{props.title}</h2>
+			</header>
+			<div className={classes.content}>
+				<p>{props.message}</p>
+			</div>
+			<footer className={classes.actions}>
+				<Button onClick={props.onDismiss}>Okay</Button>
+			</footer>
+		</Card>
+	);
+};
+
+const ErrorModal = (props) => {
+	return (
+		<>
+			{ReactDOM.createPortal(
+				<Backdrop onDismiss={props.onDismiss} />,
+				document.getElementById('backdrop-root')
+			)}
+			{ReactDOM.createPortal(
+				<ModalOverlay
+					title={props.title}
+					message={props.message}
+					onDismiss={props.onDismiss}
+				/>,
+				document.getElementById('overlay-root')
+			)}
+		</>
 	);
 };
 
