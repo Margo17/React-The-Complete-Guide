94611c6edbec8b1370771aa50c652f9b6d0ce305
Martynas Goberis <85226477+Margo17@users.noreply.github.com>
2022-07-18 18:43:27 +0300

    Add a Typical-Page folder with useEffect and useReducer hooks. Future commits will introduce more hook practice.

---
+++ Typical-Page/src/components/Login/Login.js
@@ -0,0 +1,139 @@
+import React, { useEffect, useState, useReducer } from 'react';
+
+import Card from '../UI/Card/Card';
+import classes from './Login.module.css';
+import Button from '../UI/Button/Button';
+
+const loginReducer = (state, action) => {
+	if (action.type === 'USERNAME_INPUT') {
+		return {
+			...state,
+			username: { value: action.val, isValid: action.val.includes('@') },
+		};
+	}
+	if (action.type === 'USERNAME_BLUR') {
+		return {
+			...state,
+			username: {
+				value: state.username.value,
+				isValid: state.username.value.includes('@'),
+			},
+		};
+	}
+	if (action.type === 'PASSWORD_INPUT') {
+		return {
+			...state,
+			password: { value: action.val, isValid: action.val.trim().length > 6 },
+		};
+	}
+	if (action.type === 'PASSWORD_BLUR') {
+		return {
+			...state,
+			password: {
+				value: state.password.value,
+				isValid: state.password.value.trim().length > 6,
+			},
+		};
+	}
+	return {
+		username: { value: '', isValid: null },
+		password: { value: '', isValid: null },
+	};
+};
+
+const Login = (props) => {
+	// const [enteredEmail, setEnteredEmail] = useState('');
+	// const [emailIsValid, setEmailIsValid] = useState();
+	// const [enteredPassword, setEnteredPassword] = useState('');
+	// const [passwordIsValid, setPasswordIsValid] = useState();
+	const [formIsValid, setFormIsValid] = useState(false);
+
+	const [loginState, dispatchLogin] = useReducer(loginReducer, {
+		username: { value: '', isValid: null },
+		password: { value: '', isValid: null },
+	});
+
+	const { isValid: emailIsValid } = loginState.username;
+	const { isValid: passwordIsValid } = loginState.password;
+
+	useEffect(() => {
+		const identifier = setTimeout(() => {
+			setFormIsValid(emailIsValid && passwordIsValid);
+		}, 500);
+
+		return () => {
+			clearTimeout(identifier);
+		};
+	}, [emailIsValid, passwordIsValid]);
+
+	const emailChangeHandler = (event) => {
+		dispatchLogin({ type: 'USERNAME_INPUT', val: event.target.value });
+
+		// setFormIsValid(
+		// 	event.target.value.includes('@') && loginState.password.isValid
+		// );
+	};
+
+	const passwordChangeHandler = (event) => {
+		dispatchLogin({ type: 'PASSWORD_INPUT', val: event.target.value });
+
+		// setFormIsValid(
+		// 	loginState.username.isValid && event.target.value.trim().length > 6
+		// );
+	};
+
+	const validateEmailHandler = () => {
+		dispatchLogin({ type: 'USERNAME_BLUR' });
+	};
+
+	const validatePasswordHandler = () => {
+		dispatchLogin({ type: 'PASSWORD_BLUR' });
+	};
+
+	const submitHandler = (event) => {
+		event.preventDefault();
+		props.onLogin(loginState.username.value, loginState.password.value);
+	};
+
+	return (
+		<Card className={classes.login}>
+			<form onSubmit={submitHandler}>
+				<div
+					className={`${classes.control} ${
+						loginState.username.isValid === false ? classes.invalid : ''
+					}`}
+				>
+					<label htmlFor='email'>E-Mail</label>
+					<input
+						type='email'
+						id='email'
+						value={loginState.username.value}
+						onChange={emailChangeHandler}
+						onBlur={validateEmailHandler}
+					/>
+				</div>
+				<div
+					className={`${classes.control} ${
+						loginState.password.isValid === false ? classes.invalid : ''
+					}`}
+				>
+					<label htmlFor='password'>Password</label>
+					<input
+						type='password'
+						id='password'
+						value={loginState.password.value}
+						onChange={passwordChangeHandler}
+						onBlur={validatePasswordHandler}
+					/>
+				</div>
+				<div className={classes.actions}>
+					<Button type='submit' className={classes.btn} disabled={!formIsValid}>
+						Login
+					</Button>
+				</div>
+			</form>
+		</Card>
+	);
+};
+
+export default Login;
