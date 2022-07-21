04c8c72729c33218165960b99123c1f5cf7cbcba
Martynas Goberis <85226477+Margo17@users.noreply.github.com>
2022-07-21 17:06:42 +0300

    Final updates to reducer logic, making CartItems removable and additional HeaderCartButton bump animation using useEffect hook.

--- Food-Order-App/src/components/Meals/AvailableMeals.js
+++ Food-Order-App/src/components/Meals/AvailableMeals.js
@@ -7,25 +7,25 @@ const DUMMY_MEALS = [
 		id: 'm1',
 		name: 'Sushi',
 		description: 'Finest fish and veggies',
-		price: 22.99,
+		price: 6.99,
 	},
 	{
 		id: 'm2',
 		name: 'Schnitzel',
 		description: 'A german specialty!',
-		price: 16.5,
+		price: 8.5,
 	},
 	{
 		id: 'm3',
 		name: 'Barbecue Burger',
 		description: 'American, raw, meaty.',
-		price: 12.99,
+		price: 5.59,
 	},
 	{
 		id: 'm4',
 		name: 'Green Bowl',
 		description: 'Healthy... And green...',
-		price: 18.99,
+		price: 7.4,
 	},
 ];
 
