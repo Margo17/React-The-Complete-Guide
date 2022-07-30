287ebe7d91389ad16dd924fbbf133f018ab91cd4
Martynas Goberis <85226477+Margo17@users.noreply.github.com>
2022-07-30 11:57:09 +0300

    Fix small typo in cartSlice.js

--- Redux-Cart/src/store/cartSlice.js
+++ Redux-Cart/src/store/cartSlice.js
@@ -10,7 +10,7 @@ const cartSlice = createSlice({
 	reducers: {
 		replaceCart(state, action) {
 			state.totalQuantity = action.payload.totalQuantity;
-			state.items = action.payload.item;
+			state.items = action.payload.items;
 		},
 		addItemToCart(state, action) {
 			const newItem = action.payload;
