a2d41f216d1f92b2f8103efbe5feedac94949ee3
Martynas Goberis <85226477+Margo17@users.noreply.github.com>
2022-08-08 15:43:42 +0300

    Create Next-Meetups starting pages, styles and basic components with DUMMY_DATA which will be replaced with a functional backend later.

---
+++ Next-Meetups/next-meetups/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+	return (
+		<MeetupDetail
+			image='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg'
+			title='First meetup'
+			address='Insert street 3, 12345 City'
+			description='This is a frist meetup!'
+		/>
+	);
+}
+
+export default MeetupDetails;
