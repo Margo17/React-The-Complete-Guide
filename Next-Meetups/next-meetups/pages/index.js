6984c78821134efd711399ad3e1dc1d6f833f2a5
Martynas Goberis <85226477+Margo17@users.noreply.github.com>
2022-08-09 16:23:46 +0300

    Add data fetching, head metadata, staticPaths function with parameters. Ready to be deployed in Vercel.

--- Next-Meetups/next-meetups/pages/index.js
+++ Next-Meetups/next-meetups/pages/index.js
@@ -1,34 +1,45 @@
 // my-domain.com/
 
+import Head from 'next/head';
+import { MongoClient } from 'mongodb';
+
 import MeetupList from '../components/meetups/MeetupList';
 
-const DUMMY_MEATUPS = [
-	{
-		id: 'm1',
-		title: 'A First Meetup',
-		image:
-			'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-		address: 'Insert Adress 3, 12345 City',
-		description: 'This is a first meetup!',
-	},
-	{
-		id: 'm2',
-		title: 'A Second Meetup',
-		image:
-			'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-		address: 'Insert Adress 5, 12345 City',
-		description: 'This is a second meetup!',
-	},
-];
-
 function HomePage(props) {
-	return <MeetupList meetups={props.meetups} />;
+	return (
+		<>
+			<Head>
+				<title>Next Meetups</title>
+				<meta
+					name='description'
+					content='Browse a huge list of highly active Next meetups!'
+				/>
+			</Head>
+			<MeetupList meetups={props.meetups} />
+		</>
+	);
 }
 
 export async function getStaticProps() {
+	const client = await MongoClient.connect(
+		'mongodb+srv://first-user:5S0xYUx5O09ecSf6@cluster0.geekbsn.mongodb.net/next?retryWrites=true&w=majority'
+	);
+	const db = client.db();
+
+	const meetupsCollection = db.collection('meetups');
+
+	const meetups = await meetupsCollection.find().toArray();
+
+	client.close();
+
 	return {
 		props: {
-			meetups: DUMMY_MEATUPS,
+			meetups: meetups.map((meetup) => ({
+				title: meetup.title,
+				address: meetup.address,
+				image: meetup.image,
+				id: meetup._id.toString(),
+			})),
 		},
 		revalidate: 10,
 	};
