f56978e5281744e4aca9ee9f06306e79fb3e228c
Martynas Goberis <85226477+Margo17@users.noreply.github.com>
2022-08-10 13:42:27 +0300

    Fix a bug where on initial page render the modal was appearing. Add toggle button.

--- Class-Based-Animations/src/App.js
+++ Class-Based-Animations/src/App.js
@@ -7,23 +7,43 @@ import List from './components/List/List';
 
 class App extends Component {
 	state = {
-		modalIsOpen: false,
+		modalIsOpening: null,
+		showBlock: false,
 	};
 
 	showModal = () => {
-		this.setState({ modalIsOpen: true });
+		this.setState({ modalIsOpening: true });
 	};
 
 	closeModal = () => {
-		this.setState({ modalIsOpen: false });
+		this.setState({ modalIsOpening: false });
 	};
 
 	render() {
 		return (
 			<div className='App'>
 				<h1>React Animations</h1>
-				<Modal show={this.state.modalIsOpen} closed={this.closeModal} />
-				<Backdrop show={this.state.modalIsOpen} />
+				<button
+					className='Button'
+					onClick={() =>
+						this.setState((prevState) => ({ showBlock: !prevState.showBlock }))
+					}
+				>
+					Toggle
+				</button>
+				<br />
+				{this.state.showBlock && (
+					<div
+						style={{
+							backgroundColor: 'red',
+							width: 100,
+							height: 100,
+							margin: 'auto',
+						}}
+					></div>
+				)}
+				<Modal show={this.state.modalIsOpening} closed={this.closeModal} />
+				<Backdrop show={this.state.modalIsOpening} />
 				<button className='Button' onClick={this.showModal}>
 					Open Modal
 				</button>
--- Class-Based-Animations/src/components/Modal/Modal.js
+++ Class-Based-Animations/src/components/Modal/Modal.js
@@ -3,11 +3,19 @@ import React from 'react';
 import './Modal.css';
 
 const modal = (props) => {
-	const cssClasses = ['Modal', props.show ? 'ModalOpen' : 'ModalClosed'];
+	const cssClasses = [
+		`Modal`,
+		props.show
+			? 'ModalOpening'
+			: props.show === false
+			? 'ModalClosing'
+			: 'ModalClosed',
+	];
 
 	return (
 		<div className={cssClasses.join(' ')}>
 			<h1>A Modal</h1>
+			<div></div>
 			<button className='Button' onClick={props.closed}>
 				Dismiss
 			</button>
