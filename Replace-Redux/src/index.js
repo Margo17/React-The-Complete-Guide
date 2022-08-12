0e84b2c00a54f25bcf082db9f0a5f0a7e0479074
Martynas Goberis <85226477+Margo17@users.noreply.github.com>
2022-08-12 14:24:32 +0300

    Refactor from app-wide context to a custom state management hook

--- Replace-Redux/src/index.js
+++ Replace-Redux/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import ProductsProvider from './context/productsContext';
+import configureStore from './hooksStore/productsStore';
 
 import './index.css';
 import App from './App';
 
+configureStore();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-	<ProductsProvider>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</ProductsProvider>
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
 );
