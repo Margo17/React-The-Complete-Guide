623b273cfc88b3d39558905be9efa6372a9bdf2c
Martynas Goberis <85226477+Margo17@users.noreply.github.com>
2022-08-13 21:25:47 +0300

    Add Todos component with React.FC type (just a try out, will change it later)

--- Typescript-React/src/App.tsx
+++ Typescript-React/src/App.tsx
@@ -1,7 +1,11 @@
-import './App.css';
+import Todos from './components/Todos';
 
 const App = () => {
-	return <div></div>;
+	return (
+		<div>
+			<Todos items={['Learn React', 'Learn Typescript']} />
+		</div>
+	);
 };
 
 export default App;
